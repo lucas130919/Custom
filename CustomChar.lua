6274229f35396919fe49340f6b43576baec7484a
lucas130919 <cheste1309@gmail.com>
2025-05-27 15:33:37 -0300

    CustomChar.lua

---
+++ CustomChar.lua
@@ -0,0 +1,33 @@
+local Tabela = {
+	PlayerPermission = {
+		["Bezerril1"] = 			{Tier = 2000},
+		["jobileu_avakin"] = 			{Tier = 2000},
+		["VigorOGr"] = 				{Tier = 1},
+		["jguuh"] = 				{Tier = 1},
+		["b112014"] = 				{Tier = 1},
+		["DellsLakash"] = 			{Tier = 1},
+		["Renan2809"] = 			{Tier = 1},
+		["FaustinoCB"] = 			{Tier = 1},
+		["zxnpc2"] = 				{Tier = 1},
+		["RaihNoCraK"] = 			{Tier = 1},
+	},
+	
+	CharacterTier = {
+		["Enrico"] = 					{Tier = 1,Permission = {3774843185,1193912295}},
+		["DIO"]	=					{Locked = {3774843185,1193912295}},
+		["Madara"]	=				{Locked = {3774843185,1193912295}},
+		["Koku"]	=				{Locked = {3774843185,1193912295}},
+		["Yori"]	=				{Permission = {3774843185,1193912295}},
+		["Meliodas"]	=				{Permission = {3774843185,1193912295}},
+		["Goku"]	=				{Locked = {3774843185,1193912295}},
+		["TOP"]	=					{Locked = {3774843185,1193912295}},
+		["Giorno"] = 					{Tier = 1050,Permission = {3774843185,1193912295}},
+		["Hakaishin"] = 				{Locked = {3774843185,1193912295}},
+		["Boss"] = 					{Locked = {785068132,36651812,7648333286}},
+		["JP6"] = 					{Tier = 1,Permission = {3774843185,1193912295}},
+		["Gojo"] = 					{Tier = 1,Permission = {3774843185,1193912295}}, -- perm temp
+	}
+
+}
+
+return Tabela
