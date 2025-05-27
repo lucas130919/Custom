89313aa0ff695723693b88c28bcd0a5ea56fda05
lucas130919 <cheste1309@gmail.com>
2025-05-27 15:47:39 -0300

    CustomChar.lua

--- CustomChar.lua
+++ CustomChar.lua
@@ -1,7 +1,7 @@
 local Tabela = {
 	PlayerPermission = {
 		["Bezerril1"] = 			{Tier = 2000},
-		["jobileu_avakin"] = 			{Tier = 2000},
+		["jobileu_avakin"] = 		{Tier = 2000},
 		["VigorOGr"] = 				{Tier = 1},
 		["jguuh"] = 				{Tier = 1},
 		["b112014"] = 				{Tier = 1},
@@ -13,21 +13,21 @@ local Tabela = {
 	},
 	
 	CharacterTier = {
-		["Enrico"] = 					{Tier = 1,Permission = {3774843185,1193912295}},
-		["DIO"]	=					{Locked = {3774843185,1193912295}},
-		["Madara"]	=				{Locked = {3774843185,1193912295}},
-		["Koku"]	=				{Locked = {3774843185,1193912295}},
-		["Yori"]	=				{Permission = {3774843185,1193912295}},
-		["Meliodas"]	=				{Permission = {3774843185,1193912295}},
-		["Goku"]	=				{Locked = {3774843185,1193912295}},
-		["TOP"]	=					{Locked = {3774843185,1193912295}},
-		["Giorno"] = 					{Tier = 1050,Permission = {3774843185,1193912295}},
-		["Hakaishin"] = 				{Locked = {3774843185,1193912295}},
-		["Boss"] = 					{Locked = {785068132,36651812,7648333286}},
-		["JP6"] = 					{Tier = 1,Permission = {3774843185,1193912295}},
-		["Gojo"] = 					{Tier = 1,Permission = {3774843185,1193912295}}, -- perm temp
+		["Enrico"] = 		{Tier = 1,Permission = {3774843185,1193912295}},
+		["DIO"]	=			{Locked = {3774843185,1193912295}},
+		["Madara"]	=		{Locked = {3774843185,1193912295}},
+		["Koku"]	=		{Locked = {3774843185,1193912295}},
+		["Yori"]	=		{Permission = {3774843185,1193912295}},
+		["Meliodas"]	=	{Permission = {3774843185,1193912295}},
+		["Goku"]	=		{Locked = {3774843185,1193912295}},
+		["TOP"]	=			{Locked = {3774843185,1193912295}},
+		["Giorno"] = 		{Tier = 1050,Permission = {3774843185,1193912295}},
+		["Hakaishin"] = 	{Locked = {3774843185,1193912295}},
+		["Boss"] = 			{Locked = {785068132,36651812,7648333286}},
+		["JP6"] = 			{Tier = 1,Permission = {3774843185,1193912295}},
+		["Gojo"] = 			{Tier = 1,Permission = {3774843185,1193912295}}, -- perm temp
+		["Eren"] = 			{Tier = 1,Permission = {3774843185,1193912295}}, -- adicionado conforme solicitado
 	}
-
 }
 
 return Tabela
