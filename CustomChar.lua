48243a954158a6d8275b7bb41c0eb9dae14f9fd4
lucas130919 <cheste1309@gmail.com>
2025-05-28 18:32:14 -0300

    Update CustomChar.lua

--- CustomChar.lua
+++ CustomChar.lua
@@ -2,7 +2,7 @@ local Tabela = {
 	PlayerPermission = {
 		["Bezerril1"] = 			{Tier = 2000},
 		["jobileu_avakin"] = 		        {Tier = 20000},
-		["theking130933"] = 				{Tier = 1},
+		[""] = 				{Tier = 1},
 		[""] = 				{Tier = 1},
 		[""] = 				{Tier = 1},
 		[""] = 			{Tier = 1},
@@ -20,14 +20,14 @@ local Tabela = {
 		["Yori"]	=		{Permission = {3774843185,1193912295}},
 		["Meliodas"]	=	        {Permission = {3774843185,1193912295}},
 		["Goku"]	=		{Tier = 1,Permission = {3774843185,1193912295}},
-		["TOP"]	=			{Tier = 20000,Permission = {3774843185,7237606225}},  -- destroi mapa
-		["Giorno"] = 		        {Tier = 1050,Permission = {3774843185,1193912295,7237606225}},
-		["Hakaishin"] = 	        {Tier = 20000,Permission = {3774843185,7237606225}},  -- destrói mapa "mais ou menos"
+		["TOP"]	=			{Tier = 20000,Permission = {3774843185}},  -- destroi mapa
+		["Giorno"] = 		        {Tier = 1050,Permission = {3774843185}}, -- destrói mapa "supremo"
+		["Hakaishin"] = 	        {Tier = 20000,Permission = {3774843185,1193912295}},  -- destrói mapa "mais ou menos"
 		["Boss"] = 			{Tier = 20000,Permission = {3774843185,7237606225}},  -- destrói mapa "brutal"
 		["JP6"] = 			{Tier = 1,Permission = {3774843185,1193912295}},
 		["Gojo"] = 			{Tier = 1,Permission = {3774843185,1193912295}}, -- perm temp
 		["Eren"] = 			{Tier = 1,Permission = {3774843185,1193912295}}, -- adicionado conforme solicitado
-		["Mahoraga"] = 	                {Tier = 20000,Permission = {3774843185,1193912295}}, -- novo
+		["Mahoraga"] = 	                {Tier = 20000,Permission = {3774843185,1193912295,7237606225}}, -- novo
 	}
 }
 
